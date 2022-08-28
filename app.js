84fbefc3cbc0fa16749a1be68f869a7c923eed12
Munish pthnia <manishpathania092@gmail.com>
2022-08-28 22:59:23 +0530

    add gitignore, profile,and update ports

--- app.js
+++ app.js
@@ -99,7 +99,11 @@ Item.findByIdAndRemove(checkedItemId,function(err){
 })
  });
 
+ let port = process.env.PORT;
+ if (port == null || port == "") {
+   port = 3000;
+ }
 
- app.listen(3000, function(){
-     console.log("server started on port 3000")
+ app.listen(port, function(){
+     console.log("server started sucessfully.")
  });
