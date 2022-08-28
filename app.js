b143f896f0b5787a1198e90de9801e0d9be2d830
Munish pthnia <manishpathania092@gmail.com>
2022-08-28 22:47:41 +0530

    Intial commit

---
+++ app.js
@@ -0,0 +1,105 @@
+ const express = require('express');
+ const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+
+ const app = express();
+ app.set('view engine', 'ejs')
+
+ app.use(bodyParser.urlencoded({extended: true}));
+
+ app.use(express.static("public"));
+
+ mongoose.connect("mongodb+srv://admin-Munish:text123@cluster0.1gv26b0.mongodb.net/todolistDB",{useNewUrlParser: true});
+
+ const itemsSchema = {
+     name: String
+ }
+
+ const Item = mongoose.model("Item",itemsSchema);
+
+ const item1 = new Item({
+     name : "Welcome to your todo-list"
+ });
+ const item2 = new Item({
+    name : "Hit the +  button to add a new item"
+});
+const item3 = new Item({
+    name : "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1,item2,item3];
+
+const listSchema = {
+    name: String, 
+    items: [itemsSchema]
+};
+
+
+const List = mongoose.model("List", listSchema)
+
+ app.get("/", function(req,res){
+    
+    Item.find({}, function(err, founditems){
+
+        if(founditems.length === 0){
+    Item.insertMany(defaultItems, function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("sucesss");
+    }
+});
+res.redirect("/");
+        }else{
+            res.render("list",{listTittle: "Today",newlistitems:founditems});
+        } 
+    })
+ });
+
+app.get("/:customListName",function(req,res){
+const customListName = req.params.customListName;
+
+List.findOne({name: customListName, function(err, foundList){
+    if(!err){
+        if (!foundList){
+            const list  = new List({
+                name:customListName,
+                items: defaultItems
+            });
+             list.save();
+             res.redirect("/"+customListName);
+        }else{
+            res.render("list",{listTittle: foundList.name,newListItems:foundList.items});      
+          }
+    }
+}});
+});
+
+
+ app.post("/", function(req,res){
+      const itemName = req.body.newitem;
+
+      const item  = new Item({
+          name: itemName
+      })
+      item.save();
+
+      res.redirect("/");
+ });
+
+ app.post("/delete",function(req,res){
+const checkedItemId = req.body.checkbox;
+
+Item.findByIdAndRemove(checkedItemId,function(err){
+    if(!err){
+        console.log("delete");
+        res.redirect("/");
+    }
+})
+ });
+
+
+ app.listen(3000, function(){
+     console.log("server started on port 3000")
+ });
